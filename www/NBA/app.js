99d6a82776c2637ccf3c505d3df5e53d957e1790
ctrichard <>
2021-10-13 15:06:55 +0200

    a

---
+++ www/NBA/app.js
@@ -0,0 +1,38 @@
+function CreateMaterialSelectionOptions(Team='A'){
+    let element = document.getElementById('TeamAselection'+Team);
+  
+    let x = element.length +1;
+    for (let i = 0; i <  x ; i++) {
+        element.remove(element.length-1);
+    }
+
+    let count=0;
+
+    fetch()
+
+    for (code, teamname of Object.entries(NBAData['TEAMCodes_Names'])) {
+
+      let opt = document.createElement('option');   
+      opt.appendChild( document.createTextNode(teamname) );
+      element.appendChild(opt); 
+  
+    }
+
+
+  }
+
+
+async function LoadData(){
+      
+      const NBAData = await fetch('/Data.json', {
+          headers: {
+              'Accept': 'application/json'
+          }
+      });
+}
+
+LoadData();
+
+document.getElementById('TeamAselection').addEventListener('change', (event) => {
+    selectTeam();
+});
