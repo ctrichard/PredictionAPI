88c799d1c603fa5fd658cdcd19e6f77b061fabb7
ctrichard <>
2021-10-20 15:57:08 +0200

    a

--- www/NBA/Predict.php
+++ www/NBA/Predict.php
@@ -8,12 +8,12 @@ $PlayTimePerTeam = 48*5;
 function GetPrediction($Dom,$Vis){
 
 
-    $p = shell_exec( 'whoami ');
-    print_r($p);
-    $p = shell_exec( ' env ');
-    print_r($p);
-    $p = shell_exec( ' conda env list ');
-    print_r($p);
+    // $p = shell_exec( 'whoami ');
+    // print_r($p);
+    // $p = shell_exec( ' env ');
+    // print_r($p);
+    // $p = shell_exec( ' conda env list ');
+    // print_r($p);
 
 }
 
@@ -102,7 +102,7 @@ try{
     $prediction = GetPrediction($Dom,$Vis);
     $response['Success'] = True;
     $p = shell_exec( 'conda run -n NBAPrediction python MakePrediction.py 2021-07-20 Phoenix\ Suns Milwaukee\ Bucks 456');
-    $response['Prediction'] = file_get_contents('/home/ubuntu/Projects/ParisSportifIA/Data/Prediction/Ouputs/Prediction_456.json');
+    $response['Prediction'] = json_decode(file_get_contents('/home/ubuntu/Projects/ParisSportifIA/Data/Prediction/Ouputs/Prediction_456.json'),true);
     $response['PlayerList'] = json_encode($response['PlayerList']);
 }
 catch(Exception  $e){
