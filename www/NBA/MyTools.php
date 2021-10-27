cf3cc4f8d85725ac9cd836c9b9c65a8d0fe27c0a
ctrichard <>
2021-10-27 13:37:44 +0200

    a

--- www/NBA/MyTools.php
+++ www/NBA/MyTools.php
@@ -246,7 +246,7 @@ $OutputsTempFileLocation = './Outputs/';
 
 function GetModelResults($ModelName){
     
-    $ModelResults = json_decode(file_get_contents($GLOBALS['ModelLocation'].$ModelName));
+    $ModelResults = json_decode(file_get_contents($GLOBALS['ModelLocation'].$ModelName.'ModelResults.json'));
     return $ModelResults;
 
 }
