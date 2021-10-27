37d7b79488ce64512a0fdf9ad7093c102735f045
ctrichard <>
2021-10-27 22:45:43 +0200

    a

--- www/NBA/ModelPerf.php
+++ www/NBA/ModelPerf.php
@@ -77,7 +77,7 @@ catch(Exception $a){
         G = DrawGraph.DrawStaticFunctionGraph('GraphProbRealVsEstimated',"GraphProbRealVsEstimated",Identity,parseInt(d3.select('#GraphProbRealVsEstimated').style('width'))/1);
 
         // G.setPoints(Identity)
-        let l = G.DrawLine(D,'data')
+        let l = G.DrawOtherLine(D,'data')
         console.log(l)
         // G.DrawOtherLine(Identity)
 
