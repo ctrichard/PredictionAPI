7ab7db12d71c541814e663f6b1663c67ab4c81b8
ctrichard <>
2021-11-04 17:36:22 +0200

    a

--- www/NBA/ModelPerf.php
+++ www/NBA/ModelPerf.php
@@ -85,12 +85,12 @@ catch(Exception $a){
 
         let el = d3.select('#accuracy').node()
         el.innerHTML = 'Dom Accuracy : '
-        let goodanswer = Data['ProbaGoodAnswerVsEstimatedProba']['CountDomGoodAnswer'].reduce((partial_sum, a) => partial_sum + a, 0);
+        let goodanswer = Data['ProbaGoodAnswerVsEstimatedProba']['CountDomGoodAnswer'].slice((Data['ProbaGoodAnswerVsEstimatedProba']['CountDomGoodAnswer'].length-1)/2) .reduce((partial_sum, a) => partial_sum + a, 0); //sum
         let totalaccuracy = goodanswer / Data['ProbaGoodAnswerVsEstimatedProba']['CountDomTot'].reduce((partial_sum, a) => partial_sum + a, 0);
         el.innerHTML += totalaccuracy.toFixed(2)
         el.innerHTML += '<br>' 
         el.innerHTML += 'Vis Accuracy : '
-        goodanswer = Data['ProbaGoodAnswerVsEstimatedProba']['CountVisGoodAnswer'].reduce((partial_sum, a) => partial_sum + a, 0);
+        goodanswer = Data['ProbaGoodAnswerVsEstimatedProba']['CountVisGoodAnswer']..slice((Data['ProbaGoodAnswerVsEstimatedProba']['CountVisGoodAnswer'].length-1)/2)reduce((partial_sum, a) => partial_sum + a, 0);
         totalaccuracy = goodanswer / Data['ProbaGoodAnswerVsEstimatedProba']['CountVisTot'].reduce((partial_sum, a) => partial_sum + a, 0);
         el.innerHTML += totalaccuracy.toFixed(2) 
         el.innerHTML += '<br>' 
