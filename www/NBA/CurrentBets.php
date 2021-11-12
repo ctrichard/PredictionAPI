87bbbe4073741368701bb00d37f290a5b9f74598
ctrichard <>
2021-11-12 19:47:13 +0100

    a

--- www/NBA/CurrentBets.php
+++ www/NBA/CurrentBets.php
@@ -48,7 +48,7 @@ $TomorrowDate =  date("Y-m-d",mktime(0, 0, 0, date("m")  , date("d")+1, date("Y"
     
         async function LoadData(){
                 // const PredictionData = await fetch('../Prediction/Data/PredictionData.csv', {
-                const PredictionData = await fetch('GetData?Data=PredictionData', {
+                const PredictionData = await fetch('GetData.php?Data=PredictionData', {
                     headers: {
                         'Accept': 'application/json'
                     }
