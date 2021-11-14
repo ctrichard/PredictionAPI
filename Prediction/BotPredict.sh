47c11566b8c3ebe6ed3747fde1ce772df6025d5c
ctrichard <>
2021-11-14 20:57:05 +0100

    a

--- Prediction/BotPredict.sh
+++ Prediction/BotPredict.sh
@@ -40,6 +40,8 @@ rm -rf $PathToInputs/MatchList_*
 
 env
 
+ModelNames=()
+i=0
 for BaseModelName in 'TrainWo_' 'TrainLast5Years_' 'TrainUpTo' 'TrainLast3Years_'
 do
     for ModelYear in 2005 2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022
@@ -47,15 +49,17 @@ do
         ModelName=$BaseModelName$ModelYear
         echo $ModelName
 
-        # python ~/Projects/ParisSportifIA/MakePrediction.py ${MatchDate} "$DomTeam" "$VisTeam" $UUID $PathToInputs $PathToOutputs $Season
-
-        python MakeBotPredictions.py $PathToInputs $PathToOutputs $Season $UUID $ModelName ${MatchDate}
-        # a=`grep "UUID====" $UUID_finder`
-
-        # echo $a
-
-        python StoreBotPredictions.py $UUID $BetType $PathToInputs $PathToOutputs $ModelName ${MatchDate}
+        $ModelNames[i]=$ModelName
 
     done
 done
+
+
+# python ~/Projects/ParisSportifIA/MakePrediction.py ${MatchDate} "$DomTeam" "$VisTeam" $UUID $PathToInputs $PathToOutputs $Season
+python MakeBotPredictions.py $PathToInputs $PathToOutputs $Season $UUID "$ModelName[*]" ${MatchDate}
+
+for ModelName in $ModelNames
+do
+    python StoreBotPredictions.py $UUID $BetType $PathToInputs $PathToOutputs $ModelName ${MatchDate}
+done    
 
