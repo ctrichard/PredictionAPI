e9baaad1da1207f84ff55fb5e57cb10ef13833cd
ctrichard <>
2021-11-14 15:53:57 +0100

    a

--- Prediction/BotPredict.sh
+++ Prediction/BotPredict.sh
@@ -44,7 +44,7 @@ for BaseModelName in 'TrainWo_' 'TrainLast5Years_' 'TrainUpTo' 'TrainLast3Years_
 do
     for ModelYear in 2005 2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022
     do
-        ModelName=$BaseModelNameModelYear
+        ModelName=$BaseModelName$ModelYear
         echo ModelName
     done
 done
