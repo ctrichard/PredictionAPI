54a7dd64fd0c4904fabc13d1a3d55bee7728a3bb
ctrichard <>
2021-11-14 15:54:51 +0100

    a

--- Prediction/BotPredict.sh
+++ Prediction/BotPredict.sh
@@ -45,7 +45,7 @@ do
     for ModelYear in 2005 2006 2007 2008 2009 2010 2011 2012 2013 2014 2015 2016 2017 2018 2019 2020 2021 2022
     do
         ModelName=$BaseModelName$ModelYear
-        echo ModelName
+        echo $ModelName
     done
 done
 
