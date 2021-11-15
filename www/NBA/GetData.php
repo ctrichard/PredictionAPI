804d22c0fd04734f0cf8dcffb044785f9c92b0ab
ctrichard <>
2021-11-15 18:11:52 +0100

    a

--- www/NBA/GetData.php
+++ www/NBA/GetData.php
@@ -2,6 +2,24 @@
 
 
 
+$ModelListNames = ['TrainWo_','TrainUpTo','TrainLast3Years_','TrainLast5Years_'];
+$ModelList = [];
+
+function DefineModelist(){
+
+    foreach($ModelListNames as $ModelBaseName){
+
+        foreach(range(2004,2022) as $Year){
+         
+            $GlOBALS['ModelList'].append($ModelBaseName.$Year);
+            
+        }
+    }
+
+}
+
+
+DefineModelist();
 
 
 if($_GET['Data']=='PredictionData'){
@@ -10,6 +28,20 @@ if($_GET['Data']=='PredictionData'){
     die();
 }
 
+if($_GET['Data']=='PredictionDataWithAllModels'){
 
+    $PredictionList = [];
+
+    echo $GLOBALS['ModelList'];
+    
+    foreach ($GlOBALS['ModelList'] as $ModelName){
+
+        $PredictionList[$ModelName] = file_get_contents('../../Prediction/Data/PredictionData_'.$ModelName.'.csv')
+
+    } 
+
+    echo json_encode($PredictionList);
+    die();
+}
 
 ?>
