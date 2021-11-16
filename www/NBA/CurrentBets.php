61a6fb9dcd1e7d8f21fc7c567d1856a69c2661cd
ctrichard <>
2021-11-16 13:57:33 +0100

    a

--- www/NBA/CurrentBets.php
+++ www/NBA/CurrentBets.php
@@ -190,6 +190,7 @@ $TomorrowDate =  date("Y-m-d",mktime(0, 0, 0, date("m")  , date("d")+1, date("Y"
                 })
 
                 console.log('drawing '+'data_'+BetName)
+                console.log(data)
                 // let I = G.DrawDataSet(Identity,name='Identity',type="Line",params={'color': 'lightgrey', 'strokewidth':2})
                 G.DrawDataSet(data,name='data_'+BetName,type="Histo",params={'color': 'lightgrey', 'strokewidth':1.5, 'classname' : 'BarPlot'})
 
